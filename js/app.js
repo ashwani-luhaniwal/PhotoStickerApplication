eac651b8adfa05c097fd4c49f64e7fc1c56bd02c
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2015-12-16 17:56:47 +0530

    Upload sticker images along with some js files

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
-var photoStickerApp = angular.module('PhotoStickerApp', []);
-photoStickerApp.controller('photoStickerController', function($scope) {
+var photoStickerApp = angular.module('PhotoStickerApp', ['ngFileUpload', 'LocalStorageModule']);
+
+photoStickerApp.controller('photoStickerController', ['$scope', 'Upload', '$timeout', 'localStorageService', function($scope, Upload, $timeout, localStorageService) {
 	
 	$scope.stepsModel = [];
 
 	$scope.showMainImage = false;
 	$scope.showMainImageBtn = true;
 
-	$scope.mainImageUpload = function(event) {
+	/*$scope.mainImageUpload = function(event) {
 		// var files = event.target.files; // FileList object
 		var file = event.target.files[0];
 
 		var render = new FileReader();
 		render.onload = $scope.mainImageIsLoaded;
-		alert(render.onload);
+		// alert(render.onload);
 		render.readAsDataURL(file);
 	}
 
@@ -45,9 +46,41 @@ photoStickerApp.controller('photoStickerController', function($scope) {
 		$scope.$apply(function() {
 			$scope.stepsModel.push(e.target.result);
 		});
+	}*/
+
+	for(var i=0, len=localStorage.length; i<len; i++) {
+    		var key = localStorage.key(i);
+    		var value = localStorage[key];
+    		var allUser = JSON.parse(value);
+    		$scope.stepsModel.push(allUser);
+    		console.log($scope.stepsModel);  		
+    	}
+
+	$scope.uploadPic = function(file) {
+		// file.upload = Upload.base64DataUrl(file);
+
+		if(localStorageService.isSupported) {
+    		console.log("Local Storage is supported.");
+
+    		Upload.base64DataUrl(file).then(function (url) {
+				// console.log(url);
+
+				var imgInfo = {
+					imgTitle: $scope.username,
+					imgUrl: url
+				};
+				var lsLength = localStorageService.length();
+				localStorageService.set("img" + lsLength, JSON.stringify(imgInfo));
+
+				$scope.stepsModel.push(imgInfo);
+			});
+  		}
+
+		// var localStorageItem = localStorageService.get("img0");
+		// alert(localStorageItem);
 	}
 
-});
+}]);
 
 (function() {
 	var fileInput = document.getElementById('fileInput');
